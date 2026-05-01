eb5c6dfdea284c8a7c0ad29de4b350242125c2dc
unknown <xavier.valverde101@gmail.com>
2026-04-30 23:22:26 -0500

    Cleaned up unit tests..

--- main.py
+++ main.py
@@ -28,30 +28,35 @@ def display_accounts():
     print("All accounts:")
     for row in rows:
         print(f"  ID: {row[0]}, Type: {row[1]}, Balance: {row[2]}, account_holder: {row[3]}")
-def make_account():
+def make_account(type, balance, act_hold):
     global idCounter
     idCounter += 1
-    while True:
-        type = str.lower(input("Checking or savings account: "))
-        if type != "savings" and type != "checking":
-            print("choose a savings or checking account.")
-        if type == "savings" or type == "checking":
-            break
-    while True:
-        try:
-            balance = float(input("Starting balance: "))
-        except ValueError:
-            balance = -1
-        if balance < 0:
-            print("Enter a positive number")
-        if balance > 0:
-            break
-    act_hold = input("First and last name of account holder: ")
+    if type == 0:
+        while True:
+            type = str.lower(input("Checking or savings account: "))
+            if type != "savings" and type != "checking":
+                print("choose a savings or checking account.")
+            if type == "savings" or type == "checking":
+                break
+
+    if balance == 0:
+        while True:
+            try:
+                balance = float(input("Starting balance: "))
+            except ValueError:
+                balance = -1
+            if balance < 0:
+                print("Enter a positive number")
+            if balance > 0:
+                break
+    if act_hold == 0:
+        act_hold = input("First and last name of account holder: ")
     accounts.append(act_hold)
     cursor.execute("INSERT INTO accounts VALUES (?, ?, ?, ?)", (idCounter, type, balance, act_hold))
-    print("Account made.")
+    if act_hold != 0:
+        print("Account made.")
     conn.commit()
-def deposit_account():
+def deposit_account(account, add):
     while True:
         account = input("Please enter your first and last name: ")
         if account not in accounts:
@@ -71,28 +76,33 @@ def deposit_account():
             break
     cursor.execute("UPDATE accounts SET balance = ? WHERE account_holder = ?", (bal_1[0][0] + add, account))
     print(f"New balance: {bal_1[0][0] + add}")
-def withdraw_account():
-    while True:
-        account = input("Please enter your first and last name: ")
-        if account not in accounts:
-            print("Account not found, try again.")
-        if account in accounts:
-            break
+def withdraw_account(account, withdraw):
+    if account == 0:
+        while True:
+            account = input("Please enter your first and last name: ")
+            if account not in accounts:
+                print("Account not found, try again.")
+            if account in accounts:
+                break
     bal = cursor.execute("SELECT balance FROM accounts WHERE account_holder = ?", (account,))
     bal_1 = cursor.fetchall()
-    while True:
-        try:
-            withdraw = float(input("Amount to withdraw: "))
-        except ValueError:
-            print("Enter a positive number")
-            withdraw = -1.12
-        if withdraw != -1.12 and bal_1[0][0] - withdraw < 0:
-            print("Not enough in account")
-        if withdraw != -1.12 and bal_1[0][0] - withdraw > 0:
-            break
+    if withdraw == 0:
+        while True:
+            try:
+                withdraw = float(input("Amount to withdraw: "))
+            except ValueError:
+                print("Enter a positive number")
+                withdraw = -1.12
+            if withdraw != -1.12 and bal_1[0][0] - withdraw < 0:
+                print("Not enough in account")
+            if withdraw != -1.12 and bal_1[0][0] - withdraw > 0:
+                break
     cursor.execute("UPDATE accounts SET balance = ? WHERE account_holder = ?", (bal_1[0][0] - withdraw, account))
     print(f"New balance: {bal_1[0][0] - withdraw}")
-def display_single():
+    if account == "Xotchitl Valverde":
+        global new_balance
+        new_balance = bal_1[0][0] - withdraw
+def display_single(account):
     while True:
             account = input("Please enter your first and last name: ")
             if account not in accounts:
@@ -118,19 +128,18 @@ while True:
     if select not in selection:
         print("enter an integer between 1 and 6.")
     if select == 1:
-        make_account()
+        make_account(0, 0, 0)
     if select == 2:
-        deposit_account()
+        deposit_account(0, 0)
     if select == 3:
-        withdraw_account()
+        withdraw_account(0, 0)
     if select == 4:
-        display_single()
+        display_single(0)
     if select == 5:
         print("Listing existing accounts")
         display_accounts()
     if select == 6:
         print("Thanks for using this app!")
-        conn.close()
         break
 
 '''Unit Testing'''
@@ -140,11 +149,17 @@ def does_exist(e):
 
 class TestMyFunctions(unittest.TestCase):
     def test_account_exists(self):
-        #checks that account exists
+        #checks that account exists / make account function works
+        make_account("checking", 340, "Xotchitl Valverde")
         self.assertTrue(does_exist("Xotchitl Valverde"))
+    def test_withdraw(self):
+        #checks withdraw function
+        withdraw_account("Xotchitl Valverde", 40)
+        self.assertEqual(new_balance, 300)
 '''Running The Tests'''
 import sys
 loader = unittest.TestLoader()
 suite = loader.loadTestsFromTestCase(TestMyFunctions)
 runner = unittest.TextTestRunner(stream=sys.stdout, verbosity=2)
 runner.run(suite)
+conn.close()
